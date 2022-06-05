12ea8f4e5e874c6df62441212e9b5784cd3de103
julia <julia.23js83@gmail.com>
2022-06-05 15:59:13 -0300

    app meu

---
+++ app/src/main/java/com/teste/apresentacaolayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.teste.apresentacaolayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       Toolbar toolbar=findViewById(R.id.toolbar);
+       toolbar.setTitle("Bem vindos");
+    }
+}
