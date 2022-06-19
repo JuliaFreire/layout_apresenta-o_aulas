f372c75cddb32c4b9dde9ac377aa982e800d3a92
julia <julia.23js83@gmail.com>
2022-06-19 12:04:25 -0300

    Entrada do app tela

---
+++ app/src/main/java/com/teste/apresentacaolayout/entradaAplicativo.java
@@ -0,0 +1,14 @@
+package com.teste.apresentacaolayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class entradaAplicativo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada_aplicativo);
+    }
+}
